c723b7c6038acce341e436085a0dbb6d5df000b1
Cypress4382 <cypress4382@gmail.com>
2024-09-24 15:18:25 -0600

    Updated

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -13,7 +13,7 @@ brain Brain;
 controller Controller1 = controller(primary);
 
 // Motors
-motor LeftFrontDriveMotor = motor(PORT1, ratio18_1, false);
-motor LeftRearDriveMotor = motor(PORT11, ratio18_1, false);
-motor RightFrontDriveMotor = motor(PORT10, ratio18_1, true);
-motor RightRearDriveMotor = motor(PORT20, ratio18_1, true);
+motor LeftFrontDriveMotor = motor(PORT1, ratio18_1, true);
+motor LeftRearDriveMotor = motor(PORT11, ratio18_1, true);
+motor RightFrontDriveMotor = motor(PORT10, ratio18_1, false);
+motor RightRearDriveMotor = motor(PORT20, ratio18_1, false);
